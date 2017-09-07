d091798d2eab3d854838d6af694550dc30c15ac6
koumbey <gdjadi@yahoo.com>
2017-09-07 23:19:51 +0200

    dynamique menu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import update from 'react-addons-update'
 import ReactDataGrid from 'react-data-grid';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
+const { Menu: { ContextMenu, MenuItem, SubMenu } } = require('react-data-grid-addons');
+const addons = require('react-data-grid-addons');
 
-class Example extends Component {
+
+class CustomTextInput extends Component {
+    constructor(props) {
+        super(props);
+        this.focus = this.focus.bind(this);
+    }
+
+    focus() {
+        this.textInput.focus();
+    }
+
+    render() {
+        return (
+                <input
+                    type="date"
+                    onClick={this.props.onClick}
+                    value={this.props.value}
+                    ref={(input) => { this.textInput = input; }} />
+        );
+    }
+}
+
+CustomTextInput.propTypes = {
+    onClick: React.PropTypes.func,
+    value: React.PropTypes.string
+};
+
+class ExampleEditor extends Component {
     constructor(props) {
         super(props);
         this.state = {
             startDate: moment(props.value, 'YYYY-MM-DD')
         };
         this.handleChange = this.handleChange.bind(this);
+        this.getInputNode = this.getInputNode.bind(this);
+        this.getValue = this.getValue.bind(this);
+    }
+
+    componentDidMount(){
+        console.log(this.getValue());
+    }
+    getInputNode(){
+        return this.customInput;
+    }
+
+    getValue(){
+        return this.state.startDate["_i"];
     }
 
     handleChange(date) {
@@ -24,6 +66,7 @@ class Example extends Component {
 
     render() {
         return <DatePicker
+            customInput ={<CustomTextInput/>}
             dateFormat = "YYYY-MM-DD"
             selected={this.state.startDate}
             onChange={this.handleChange}
@@ -44,23 +87,175 @@ class Example extends Component {
     }
 }
 
+const MyContextMenu = React.createClass({
+    propTypes: {
+        onRowDelete: React.PropTypes.func.isRequired,
+        onRowInsertAbove: React.PropTypes.func.isRequired,
+        onRowInsertBelow: React.PropTypes.func.isRequired,
+        rowIdx: React.PropTypes.string.isRequired,
+        idx: React.PropTypes.string.isRequired,
+        selectedRow : React.PropTypes.object.isRequired
+    },
 
-const columns = [
-    { key: 'First', name: 'First' },
-    { key: 'second', name: 'Second' },
-    { key: 'data', name: 'Date', editable: true, formatter: <Example/>}
-];
-const rows = [
-{ First: "Toto", second: 'Toti', data: "2017-08-20" },
-{ First: "Tonton", second: 'Garba', data: "2017-07-20" },
-{ First: "Ali", second: 'Abdou', data: "2017-06-20" },
-{ First: "Omar", second: 'Tankari', data: "2017-05-10" },
-{ First: "Omar", second: 'Tounkou', data: "2017-05-20" },
-{ First: "Nana", second: 'Gomma', data: "2017-08-11" },
-{ First: "Tante", second: 'Didi', data: "2017-04-01" }
-];
+    getInitialState(){
+        return {conditions: [false, false, false]}
+    },
+    onRowDelete(e, data) {
+        if (typeof(this.props.onRowDelete) === 'function') {
+            this.props.onRowDelete(e, data);
+        }
+    },
 
-const rowGetter = rowNumber => rows[rowNumber];
+    onRowInsertAbove(e, data) {
+        if (typeof(this.props.onRowInsertAbove) === 'function') {
+            this.props.onRowInsertAbove(e, data);
+        }
+    },
+
+    onRowInsertBelow(e, data) {
+        if (typeof(this.props.onRowInsertBelow) === 'function') {
+            this.props.onRowInsertBelow(e, data);
+        }
+    },
+
+    componentWillReceiveProps(newParam){
+        if(newParam.selectedRow.rowId !== -1){
+            console.log("hello :"+ newParam.selectedRow.rowId);
+           if (newParam.selectedRow.rowId !== this.props.selectedRow.rowId ){
+               let obj = newParam.selectedRow.row;
+               let cond = [];
+               cond.push(obj["id"] %2 ===0);
+               cond.push(obj["title"].length <8);
+               cond.push(obj["count"] <5000);
+               this.setState({conditions: cond})
+           }
+        } else {
+            this.setState({conditions: [false, false, false]})
+        }
+    },
+    render() {
+        return (
+            <ContextMenu>
+                {this.state.conditions[0] && <MenuItem data={{rowIdx: this.props.selectedRow, idx: this.props.idx}} onClick={this.onRowDelete}>Delete Row</MenuItem>}
+                {this.state.conditions[1] && <MenuItem data={{rowIdx: this.props.rowIdx, idx: this.props.idx}} onClick={this.onRowInsertAbove}>Above</MenuItem>}
+                {this.state.conditions[3] && <MenuItem data={{rowIdx: this.props.rowIdx, idx: this.props.idx}} onClick={this.onRowInsertBelow}>Below</MenuItem>}
+            </ContextMenu>
+        );
+    }
+});
+
+const Example = React.createClass({
+    getInitialState() {
+        this._columns = [
+            { key: 'id', name: 'ID' },
+            { key: 'title', name: 'Title' },
+            { key: 'count', name: 'Count' }
+        ];
+
+        let rows = [];
+        for (let i = 1; i < 1000; i++) {
+            rows.push({
+                id: i,
+                title: 'Title ' + i,
+                count: i * 1000
+            });
+        }
+
+        return { rows : rows , selectedIndexes: [], selectedRow:{row:{}, rowId:-1}};
+    },
+    getData(){
+        let rows = [];
+        for (let i = 1; i < 1000; i++) {
+            rows.push({
+                id: i,
+                title: 'Title ' + i,
+                count: i * 1000
+            });
+        }
+
+        return rows;
+    },
+
+    rowGetter(rowIdx) {
+        return this.state.rows[rowIdx];
+    },
+
+    deleteRow(e, { rowIdx }) {
+        this.state.rows.splice(rowIdx, 1);
+        this.setState({rows: this.state.rows});
+    },
+
+    insertRowAbove(e, { rowIdx }) {
+        this.insertRow(rowIdx);
+    },
+
+    insertRowBelow(e, { rowIdx }) {
+        this.insertRow(rowIdx + 1);
+    },
+
+    insertRow(rowIdx) {
+        const newRow = {
+            id: 0,
+            title: 'New at ' + (rowIdx + 1),
+            count: 0
+        };
+
+        let rows = [...this.state.rows];
+        rows.splice(rowIdx, 0, newRow);
+
+        this.setState({ rows });
+    },
+
+    onRowsSelected(rows) {
+        if (this.state.selectedIndexes.length === 0) {
+            this.setState({selectedIndexes: this.state.selectedIndexes.concat(rows.map(r => r.rowIdx)),
+            selectedRow : {row :rows[0].row, rowIdx: rows[0].rowIdx}});
+
+        } else {
+            this.setState({selectedIndexes: this.state.selectedIndexes.concat(rows.map(r => r.rowIdx)),
+            selectedRow: {row:{}, rowId:-1}});
+        }
+
+    },
+
+    onRowsDeselected(rows) {
+        let rowIndexes = rows.map(r => r.rowIdx);
+        let leftIndex = this.state.selectedIndexes.filter(i => rowIndexes.indexOf(i) === -1 );
+        if (leftIndex.length === 1){
+            this.setState({selectedIndexes: leftIndex, selectedRow: {row :this.state.rows[leftIndex[0]], rowId:leftIndex[0]}});
+        } else{
+            this.setState({selectedIndexes: leftIndex, selectedRow: {row:{}, rowId:-1}});
+        }
+
+
+    },
+
+    render() {
+        return (
+            <ReactDataGrid
+                contextMenu={<MyContextMenu selectedRow={this.state.selectedRow}
+                                            onRowDelete={this.deleteRow}
+                                            onRowInsertAbove={this.insertRowAbove}
+                                            onRowInsertBelow={this.insertRowBelow}
+                           />}
+                columns={this._columns}
+                rowGetter={this.rowGetter}
+                rowsCount={this.state.rows.length}
+                minHeight={500} rowSelection={{
+                showCheckbox: true,
+                enableShiftSelect: true,
+                onRowsSelected: this.onRowsSelected,
+                onRowsDeselected: this.onRowsDeselected,
+                selectBy: {
+                    indexes: this.state.selectedIndexes
+                }}}/>
+        );
+    }
+});
+
+
+
+
 
 class App extends Component {
   render() {
@@ -74,11 +269,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
           <div id ="tabs">
-              <ReactDataGrid
-                  columns={columns}
-                  rowGetter={rowGetter}
-                  rowsCount={rows.length}
-                  minHeight={500} />);
+              <Example/>
           </div>
       </div>
     );
